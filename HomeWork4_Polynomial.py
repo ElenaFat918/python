8b1edc1acdf295120a61ddea3c50f28d0e7526e7
Elena <keaya@rambler.ru>
2022-10-08 20:23:58 +0300

    Задача №4. Программа, формирующая случайный список коэффициентов многочлена и записывающая в файл многочлен степени k

---
+++ HomeWork4_Polynomial.py
@@ -0,0 +1,27 @@
+# Задача №4. Задана натуральная степень k. Сформировать случайным образом список
+# коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен
+# степени k.
+
+# Пример:   k=2 => 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+
+print('Программа, формирующая случайный список коэффициентов многочлена и записывающая в файл многочлен степени k')
+k = int(input('Введите натуральную степень k '))
+sp = []
+
+import random
+
+for i in range(0, k+1):
+    i = random.randint(0, 100)
+    sp.append(i)
+print('Получился следующий список коэффициентов многочлена: ', sp)
+x = 'x' 
+member = []
+for i in sp: 
+    i = str(i) + str(x) + '^' + str(k) 
+    member.append(i) 
+    k -= 1
+polynomial = '+'.join(member)
+print('Сформирован и записан в файл listOfCoefficients.txt многочлен', polynomial)
+with open('listOfCoefficients.txt', 'w') as data:
+    data.write(polynomial)
