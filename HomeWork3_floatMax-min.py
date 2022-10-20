cad05b579b297e826708c0d06cd5589b01859184
Elena <keaya@rambler.ru>
2022-10-20 22:51:48 +0300

    Алтернативное решение

--- HomeWork3_floatMax-min.py
+++ HomeWork3_floatMax-min.py
@@ -8,11 +8,15 @@
 print('Поиск разницы между максимальным и минимальным значением дробной части элементов')
 sp = [1.1, 1.2, 3.1, 5, 10.01] 
 print(sp)
-min = 1 
-max = 0 
-for i in sp:
-    if (i - int(i)) <= min: 
-        min = i - int(i) 
-    if (i - int(i)) >= max: 
-        max = i - int(i)   
-print('\nРазность между максимальным и минимальным значением дробной части элементов равна', round(max-min, 2))
+# min = 1 
+# max = 0 
+# for i in sp:
+#     if (i - int(i)) <= min: 
+#         min = i - int(i) 
+#     if (i - int(i)) >= max: 
+#         max = i - int(i)   
+# print('\nРазность между максимальным и минимальным значением дробной части элементов равна', round(max-min, 2))
+
+sp_frac = [i - int(i) for i in sp if int(i) != i]   # созжаем список дроных частей
+print(sp_frac)
+print(max(sp_frac) - min(sp_frac))
