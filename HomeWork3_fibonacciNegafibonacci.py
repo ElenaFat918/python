76ddbdde0bc789ca56ad5a1e94eb11aded44d9b4
Elena <keaya@rambler.ru>
2022-10-24 10:44:29 +0300

    Альтернативне решение из семинара

--- HomeWork3_fibonacciNegafibonacci.py
+++ HomeWork3_fibonacciNegafibonacci.py
@@ -29,3 +29,11 @@ for i in range(1, k + 1):
      sp.append(Fibonacci(i)) 
      sp.insert(0, NegaFibonacci(i)) 
 print(f'Для k = {k} список будет выглядеть так:\n', sp)
+# 
+# n = int(input("Введите  число:\n"))
+# fib = [0, 1]
+# for i in range(2, n + 1):
+#     fib.append(fib[-1] + fib[-2])
+# for i in range(n):
+#     fib = [fib[1] - fib[0]] + fib
+# print(fib)
